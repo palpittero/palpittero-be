ac412bb445a13b951274934fbf9ed44af2c41726
Pablo Veiga <vcpablo@gmail.com>
2022-09-20 21:31:24 -0300

    chore: adjust deployment config

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -4,12 +4,12 @@ const sendEmail = ({ to, from = process.env.EMAIL_FROM, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_SERVICE_HOST,
     port: Number(process.env.SMTP_SERVICE_PORT),
-    secure: Boolean(process.env.SMTP_SERVICE_SECURE),
+    secure: true,
     auth: {
       user: process.env.SMTP_USER_NAME,
       pass: process.env.SMTP_USER_PASSWORD
-    },
-    tls: { rejectUnauthorized: false }
+    }
+    // tls: { rejectUnauthorized: false }
   })
 
   const mailOptions = {
@@ -20,16 +20,16 @@ const sendEmail = ({ to, from = process.env.EMAIL_FROM, subject, html }) => {
   }
 
   return new Promise((resolve, reject) => {
-    console.log('Trying to send account activation e-mail')
+    console.log('Trying to send e-mail')
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log('Error sending e-mail', error.toString())
         reject(error)
+      } else {
+        console.log('E-mail sucessfully sent', info)
+        resolve(info)
       }
-
-      console.log('E-mail sucessfully sent', info)
-      resolve(info)
     })
   })
 }
