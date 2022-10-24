86187502320cefc12fe1772631d4b95d8b62efc0
Pablo Veiga <vcpablo@gmail.com>
2022-10-24 19:54:15 -0300

    CU-345cfa1 - Unprocessed guesses query should only consider finished matches

---
+++ src/modules/matches/matches.constants.js
@@ -0,0 +1,12 @@
+export const MATCH_STATUSES = {
+  SCHEDULED: 'scheduled',
+  PREPARATION: 'preparation',
+  IN_PROGRESS: 'in_progress',
+  REGULAR_TIME: 'regularTime',
+  EXTRA_TIME: 'extraTime',
+  PENALTIES: 'penalties',
+  FINISHED: 'finished',
+  CANCELLED: 'cancelled',
+  POSTPONED: 'postponed',
+  DELETED: 'deleted'
+}
