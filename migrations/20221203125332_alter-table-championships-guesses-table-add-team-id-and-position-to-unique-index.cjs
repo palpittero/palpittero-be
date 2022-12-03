fefd79a4f26fe0bfd9d425631f14c373544e337b
Pablo Veiga <vcpablo@gmail.com>
2022-12-03 09:56:56 -0300

    fix(guesses): fix copy championships guesses

---
+++ migrations/20221203125332_alter-table-championships-guesses-table-add-team-id-and-position-to-unique-index.cjs
@@ -0,0 +1,106 @@
+module.exports = {
+  up: function (knex) {
+    return knex.schema.alterTable('championshipsGuesses', function (table) {
+      table.dropForeign('userId')
+      table.dropForeign('championshipId')
+      table.dropForeign('leagueId')
+      table.dropForeign('teamId')
+
+      table.dropUnique(
+        ['championshipId', 'leagueId', 'userId'],
+        'championshipsguesses_unique_index'
+      )
+
+      table
+        .integer('userId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .alter()
+      table
+        .integer('championshipId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('championships')
+        .onDelete('CASCADE')
+        .alter()
+      table
+        .integer('leagueId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('leagues')
+        .onDelete('CASCADE')
+        .alter()
+      table
+        .integer('teamId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('teams')
+        .onDelete('CASCADE')
+        .alter()
+
+      table.unique(
+        ['championshipId', 'leagueId', 'userId', 'teamId', 'position'],
+        'championshipsguesses_unique_index'
+      )
+    })
+  },
+
+  down: function (knex) {
+    return knex.schema.alterTable('championshipsGuesses', function (table) {
+      table.dropForeign('championshipId')
+      table.dropForeign('leagueId')
+      table.dropForeign('userId')
+      table.dropForeign('teamId')
+
+      table.dropUnique(
+        ['championshipId', 'leagueId', 'userId', 'teamId', 'position'],
+        'championshipsguesses_unique_index'
+      )
+
+      table
+        .integer('userId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .alter()
+      table
+        .integer('championshipId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('championships')
+        .onDelete('CASCADE')
+        .alter()
+      table
+        .integer('leagueId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('leagues')
+        .onDelete('CASCADE')
+        .alter()
+
+      table
+        .integer('teamId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('leagues')
+        .onDelete('CASCADE')
+        .alter()
+
+      table.unique(
+        ['championshipId', 'leagueId', 'userId'],
+        'championshipsguesses_unique_index'
+      )
+    })
+  }
+}
