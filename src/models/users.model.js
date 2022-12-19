107907e44cc4d3e27ca96003773330d38c50ee21
Pablo Veiga <vcpablo@gmail.com>
2022-12-19 13:45:56 -0300

    feat(guesses): fix user ranking

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -3,6 +3,7 @@ import omitBy from 'lodash/fp/omitBy'
 import isNil from 'lodash/fp/isNil'
 import baseModel from './base.model'
 import omit from 'lodash/fp/omit'
+import orderBy from 'lodash/fp/orderBy'
 import { STATUS } from '../shared/constants'
 
 const TABLE_NAME = 'users'
@@ -33,38 +34,45 @@ const fetchByLeague = async ({ leagueId, status }) => {
   const rows = await knex(TABLE_NAME)
     .select([
       `${TABLE_NAME}.*`,
-      knex.raw('COALESCE(SUM(guesses.points), 0) AS points'),
+      // knex.raw(
+      //   'COALESCE(SUM(championshipsGuesses.points), 0) AS championshipsGuessesPoints'
+      // ),
       'usersLeagues.status AS usersLeaguesStatus',
-      'usersLeagues.owner AS usersLeaguesOwner'
+      'usersLeagues.owner AS usersLeaguesOwner',
+      knex.raw(
+        '(SELECT(COALESCE(SUM(points), 0)) FROM guesses WHERE userId = users.id AND leagueId = usersLeagues.leagueId) AS guessesPoints'
+      ),
+      knex.raw(
+        '(SELECT(COALESCE(SUM(points), 0)) FROM championshipsGuesses WHERE userId = users.id AND leagueId = usersLeagues.leagueId) AS championshipsGuessesPoints'
+      )
     ])
     .join('usersLeagues', 'usersLeagues.userId', `${TABLE_NAME}.id`)
-    .leftJoin('guesses', function () {
-      this.on('guesses.userId', '=', `usersLeagues.userId`).andOn(
-        'guesses.leagueId',
-        '=',
-        'usersLeagues.leagueId'
-      )
-    })
     .where(appendWhere({ leagueId, status }))
-    .groupBy(`${TABLE_NAME}.id`)
-    .orderBy('points', 'desc')
-
   return appendEntities(rows)
 }
 
 const appendEntities = (rows) => {
-  const USERS_LEAGUES_FIELDS = [
-    'usersLeaguesStatus',
-    'usersLeaguesOwner',
-    'points'
-  ]
+  const USERS_LEAGUES_FIELDS = ['usersLeaguesStatus', 'usersLeaguesOwner']
 
-  return rows.map((row) => ({
-    ...omit(USERS_LEAGUES_FIELDS, row),
-    status: row.usersLeaguesStatus,
-    owner: row.usersLeaguesOwner,
-    points: parseInt(row.points || 0)
-  }))
+  return orderBy(
+    'points',
+    'desc',
+    rows.map((row) => {
+      const guessesPoints = parseInt(row.guessesPoints || 0)
+      const championshipsGuessesPoints = parseInt(
+        row.championshipsGuessesPoints || 0
+      )
+
+      const points = guessesPoints + championshipsGuessesPoints
+
+      return {
+        ...omit(USERS_LEAGUES_FIELDS, row),
+        status: row.usersLeaguesStatus,
+        owner: row.usersLeaguesOwner,
+        points
+      }
+    })
+  )
 }
 
 const appendWhere = ({ leagueId, status }) =>
