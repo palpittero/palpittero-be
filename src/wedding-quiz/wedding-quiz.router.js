c61874c7f2302f14b99cf47bb6addcce262020d6
Pablo Veiga <vcpablo@gmail.com>
2023-07-20 21:04:36 -0300

    feat(wedding-quiz): introduce wedding quiz auth

--- src/wedding-quiz/wedding-quiz.router.js
+++ src/wedding-quiz/wedding-quiz.router.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
-import { login, logout } from './wedding-quiz.controller.js'
+import { login, logout, me } from './wedding-quiz.controller.js'
 
 const router = Router()
 
 router.get('/', (req, res) => res.status(200).json({ status: 'online' }))
 router.post('/auth/login', login)
 router.post('/auth/logout', logout)
+router.get('/auth/me', me)
 
 export default router
