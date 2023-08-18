2f0e933597fedf095d51718d561e729b93d49cb1
Pablo Veiga <vcpablo@gmail.com>
2023-08-17 23:41:35 -0300

    feat(wedding-quiz): fix queries and authentication

--- src/wedding-quiz/wedding-quiz.router.js
+++ src/wedding-quiz/wedding-quiz.router.js
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 
-import { login, logout, me } from './wedding-quiz.controller.js'
+import {
+  login,
+  magicLogin,
+  magicLogout,
+  me
+} from './wedding-quiz.controller.js'
 
 const router = Router()
 
 router.get('/', (req, res) => res.status(200).json({ status: 'online' }))
 router.post('/auth/login', login)
-router.post('/auth/logout', logout)
+router.post('/auth/magic-login', magicLogin)
+router.post('/auth/magic-logout', magicLogout)
 router.get('/auth/me', me)
 
 export default router
