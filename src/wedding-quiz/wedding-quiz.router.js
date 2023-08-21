eeaa70152aa313c95e5a587e0c818785762612d3
Pablo Veiga <vcpablo@gmail.com>
2023-08-20 21:08:33 -0300

    feat(wedding-quiz): disable magic

--- src/wedding-quiz/wedding-quiz.router.js
+++ src/wedding-quiz/wedding-quiz.router.js
@@ -2,8 +2,8 @@ import { Router } from 'express'
 
 import {
   login,
-  magicLogin,
-  magicLogout,
+  // magicLogin,
+  // magicLogout,
   me
 } from './wedding-quiz.controller.js'
 
@@ -11,8 +11,8 @@ const router = Router()
 
 router.get('/', (req, res) => res.status(200).json({ status: 'online' }))
 router.post('/auth/login', login)
-router.post('/auth/magic-login', magicLogin)
-router.post('/auth/magic-logout', magicLogout)
+// router.post('/auth/magic-login', magicLogin)
+// router.post('/auth/magic-logout', magicLogout)
 router.get('/auth/me', me)
 
 export default router
