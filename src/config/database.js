423f733fc76ca8681597e94141db946601d90c55
Pablo Veiga <pablo@spectora.com>
2025-08-12 22:58:26 -0300

    feat: attempt mysql conf

--- src/config/database.js
+++ src/config/database.js
@@ -5,14 +5,15 @@ const { NODE_ENV } = process.env
 
 const database = knex(knexfile[NODE_ENV])
 
-if (NODE_ENV === 'memory') {
-  await database.raw('PRAGMA foreign_keys = ON')
-} else {
-  await database.raw(`SET SESSION time_zone = '-3:00';`)
-  await database.raw(`SET SESSION time_zone = '-3:00';`)
-  await database.raw(
-    `SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));`
-  )
+export const configureSession = async () => {
+  if (NODE_ENV === 'memory') {
+    await database.raw('PRAGMA foreign_keys = ON')
+  } else {
+    await database.raw(`SET SESSION time_zone = '-3:00';`)
+    await database.raw(
+      `SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));`
+    )
+  }
 }
 
 export default database
